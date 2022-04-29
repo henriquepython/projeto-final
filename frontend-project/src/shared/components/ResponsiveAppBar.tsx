990dfdf39eb11e4328b2ea0b326dc9d84cdf2d47
Henriquepython <joaohbp59@gmail.com>
2022-04-29 10:19:40 -0300

    refactor: header finalizado

--- frontend-project/src/shared/components/ResponsiveAppBar.tsx
+++ frontend-project/src/shared/components/ResponsiveAppBar.tsx
@@ -3,17 +3,16 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import SettingsIcon from '@mui/icons-material/Settings';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LoginIcon from '@mui/icons-material/Login';
-import SearchIcon from '@mui/icons-material/Search';
-import { Input, TextField } from '@mui/material';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Divider, Input, Typography } from '@mui/material';
 
 interface HeaderProps {
   sections: ReadonlyArray<{
@@ -38,24 +37,17 @@ export const ResponsiveAppBar = (props: HeaderProps) => {
  
   return (
     <AppBar 
-    position="fixed" sx={{ p: 1, background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,119,1) 35%, rgba(0,212,255,1) 100%)'}}>
+    position="fixed" sx={{ p: 1, background: 'none', color: 'black'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <IconButton
-            href='/home'
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, color: 'white' }}
-          >
-            {title}
-          </IconButton>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: 'flex' }}>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
+              color='inherit'
               onClick={handleOpenNavMenu}
-              color="inherit"
             >
               <MenuIcon />
             </IconButton>
@@ -73,69 +65,85 @@ export const ResponsiveAppBar = (props: HeaderProps) => {
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
+              color='inherit'
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: 'block',
               }}
             >
+              <Typography 
+                color='inherit'
+                sx={{ ml: 2, my: 1 }}  
+              >
+                Category
+              </Typography>
+              <Divider color= 'black' />
               {sections.map((section) => (
                 <Button 
                 key={section.title}
                 href={section.url}
                 onClick={handleCloseNavMenu}
-                sx={{ display: { xs: 'flex'} }}
+                variant='outlined'
+                color='inherit'
+                sx={{ display: { xs: 'flex'}, mx: 4, my: 2  }}
                 >
                     {section.title}
                 </Button>
               ))}
             </Menu>
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {sections.map((section) => (
-              <Button
-                key={section.title}
-                onClick={handleCloseNavMenu}
-                variant='outlined'
-                href={section.url}
-                sx={{ my: 2, m: 1, color: 'white', display: 'block', borderColor: 'white', fontSize: 13}}
-              >
-                {section.title}
-              </Button>
-            ))}
-          </Box>
-          
-          <Input id="searchProduct" type="search" placeholder='Search Products' sx={{ mr: 1, color: 'white', position: 'relative'}} />
 
-          <IconButton sx={{ flexGrow: 0, mr: 14, color: 'white', display: { xs: 'none', md: 'flex' }}}>
-            <SearchIcon />
-          </IconButton> 
+            <IconButton
+            href='/home'
+            color='inherit'
+            sx={{ ml: 2 }}
+            >
+              {title}
+            </IconButton>
+          </Box>
+         
 
-          <IconButton sx={{ flexGrow: 0, mr: 1, color: 'white', display: { xs: 'flex', md: 'none' }}}>
-            <SearchIcon />
+          <Input 
+            id="searchProduct"
+            type="search"
+            placeholder='Search Products'
+            sx={{ position: 'relative', display: { xs: 'none', md: 'flex'}, width: '40%'}} 
+          />
+
+          <IconButton 
+            color='inherit'
+            sx={{ flexGrow: 0, mr: 19, display: { xs: 'none', md: 'flex' }}}
+          >
+            <SearchOutlinedIcon />
           </IconButton> 
 
           <Box sx={{ flexGrow: 0, mr: .5}}>
             <Tooltip title="Login">
               <IconButton 
-                sx={{color: 'white'}}
                 href='/signin'
+                color='inherit'
               >
-                <LoginIcon />
+                <AccountCircleOutlinedIcon />
               </IconButton>
             </Tooltip>
           </Box>   
           
           <Box sx={{ flexGrow: 0,mr: .5 }}>
             <Tooltip title="Open Cart">
-              <IconButton href='/cart'>
-                <ShoppingCartIcon sx={{color: 'white'}} />
+              <IconButton 
+                href='/cart'
+                color='inherit'
+              >
+                <ShoppingCartOutlinedIcon />
               </IconButton>
             </Tooltip>
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open Area Admin">
-              <IconButton href='/signinadmin'>
-                <SettingsIcon sx={{color: 'white'}}/>
+              <IconButton 
+                href='/signinadmin'
+                color='inherit'
+              >
+                <SettingsOutlinedIcon />
               </IconButton>
             </Tooltip>
           </Box>
